6c2614fd3a7b44d0082d08f3db07760081667b4a
Desmond <cc.chin@techninier.com>
2019-05-06 22:29:08 +0800

    Added ability to have components within another component

--- index.js
+++ index.js
@@ -1,27 +1,57 @@
 class Popcorn{
-    constructor(DOMWrapper, initialState = {}){
+    constructor(DOMWrapper=false, initialState = {}){
         this.DOMWrapper = DOMWrapper;
         this.DOMTrackers = {};
+        if(typeof DOMWrapper === 'object' || DOMWrapper === false)
+            initialState = DOMWrapper
         this.state = initialState;
 
-        this.Render = this.render();
-        this.createDOM();
+        /**
+         * Triggers the willBeRendered function
+         * right before rendering is to take place
+         */
+        if(this.willBeRendered)
+            this.willBeRendered();
+
+        /**
+         * Checks if the render() method exists
+         */
+        if(this.render)
+            this.Render = this.render();
+        else
+            throw "Fatal: Every component needs to have a render method";
+
+        /**
+         * Checks if the render() method returns anything useful or at all
+         */
+        if(this.Render){
+            this.createDOM();
+        }else throw "Fatal: Every render needs to return an interface";
+        
+        /**
+         * Triggers the hasRendered() method if found
+         */
+        if(this.hasRendered)
+            this.hasRendered();
     }
 
     createDOM(){
         let toRender = `<popcornDOM>${this.Render}</popcornDOM>`;
 
-        let DOMs = document.querySelectorAll(this.DOMWrapper);
-        for(let i=0; i<DOMs.length; i++){
-            DOMs[i].innerHTML = toRender;
+        if(typeof this.DOMWrapper === 'object' || this.DOMWrapper === false){
+            return toRender;
+        }else{
+            let DOMs = document.querySelectorAll(this.DOMWrapper);
+            for(let i=0; i<DOMs.length; i++){
+                DOMs[i].innerHTML = toRender;
+            }   
         }
-
-        if(this.hasRendered)
-            this.hasRendered();
     }
 
-
     initiateState(states){
+        /**
+         * This method only allow state creation when states are empty
+         */
         let isEmpty = true;
         for(var prop in this.state) {
             if(this.state.hasOwnProperty(prop)) {
@@ -41,12 +71,11 @@ class Popcorn{
         for(var prop in states) {
             this.state[prop] = states[prop];
         }
-        this.Render = this.render();
-        
-        this.createDOM();
-    }
 
-    render(){
-        console.log("Render not found");
+        /**
+         * To rerender after setState
+         */
+        this.Render = this.render();
+        this.createDOM();
     }
 }
